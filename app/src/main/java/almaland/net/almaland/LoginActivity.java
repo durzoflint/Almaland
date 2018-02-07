f12d5ab7cab20bca3f97572c468a7335020c3d8d
durzoflint <abhinavu1201@gmail.com>
2018-02-07 21:08:12 +0530

    Integrated Linkedin Login

--- app/src/main/java/almaland/net/almaland/LoginActivity.java
+++ app/src/main/java/almaland/net/almaland/LoginActivity.java
@@ -1,7 +1,15 @@
 package almaland.net.almaland;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +17,46 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // Authenticate with linkedin and intialize Session.
+
+    public void login(View view){
+        LISessionManager.getInstance(getApplicationContext())
+                .init(this, buildScope(), new AuthListener() {
+                    @Override
+                    public void onAuthSuccess() {
+
+                        Toast.makeText(getApplicationContext(), "success" +
+                                        LISessionManager
+                                                .getInstance(getApplicationContext())
+                                                .getSession().getAccessToken().toString(),
+                                Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onAuthError(LIAuthError error) {
+
+                        Toast.makeText(getApplicationContext(), "failed "
+                                        + error.toString(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                }, true);
+    }
+
+    /*// handle the respone by calling LISessionManager and start new activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        LISessionManager.getInstance(getApplicationContext())
+                .onActivityResult(this,
+                        requestCode, resultCode, data);
+
+        Toast.makeText(this, "Login Success?", Toast.LENGTH_SHORT).show();
+    }*/
+
+    // set the permission to retrieve basic information of User's linkedIn account
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.R_EMAILADDRESS);
     }
 }
